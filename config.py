b810be60e5be40f0bb02d27dbac27e15d8abbc09
SaintCR <samuelsaldarriaga0510@gmail.com>
2025-09-23 21:14:06 -0500

    Configuración realizada para su conexión a su nuevo discord

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv()
 # ---------------- CONFIGURACIÓN DEL BOT ----------------
 TOKEN = os.getenv("DISCORD_TOKEN")
 COMMAND_PREFIX = "!"
-CANAL_SERVICIO = 1416555773970088104  # ID del canal donde se usa !servicio
-CANAL_TOP_DOMINGO = 1416803772667133962  # ID del canal donde se publicará el top semanal
+CANAL_SERVICIO = 1419357335645720588  # ID del canal donde se usa !servicio
+CANAL_TOP_DOMINGO = 1419357447444893877  # ID del canal donde se publicará el top semanal
 ARCHIVO_DATOS = "servicios.json"
 ROL_SERVICIO = "En Servicio"
